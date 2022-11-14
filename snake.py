005d20d223923ea3c6ddea5a5e9ddc7575e0b39c
Justin Cole <jcole429@student.fdu.edu>
2022-11-14 09:49:35 -0500

    Setting up listeners for snake direction control

--- snake.py
+++ snake.py
@@ -2,6 +2,10 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -9,6 +13,7 @@ class Snake:
 
     def __init__(self):
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -22,4 +27,20 @@ class Snake:
         for seg_num in range(len(self.segments) - 1, 0, -1):
             previous_seg_position = self.segments[seg_num - 1].position()
             self.segments[seg_num].goto(previous_seg_position)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
