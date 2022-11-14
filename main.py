d155b500f422f7dd51ae8a8466ffae3bdb3769a6
Justin Cole <jcole429@student.fdu.edu>
2022-11-14 09:31:22 -0500

    Refactoring snake code into Snake class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,26 +8,13 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.2)
-
-    for seg_num in range(len(segments) - 1, 0, -1):
-        previous_seg_position = segments[seg_num-1].position()
-        segments[seg_num].goto(previous_seg_position)
-    segments[0].forward(20)
+    snake.move()
 
 screen.exitonclick()
