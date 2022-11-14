5a7affb1003ad91e9ab551686445b68123e85d24
Justin Cole <jcole429@student.fdu.edu>
2022-11-14 16:36:48 -0500

    Growing segments when eating food and detecting tail collisions

--- snake.py
+++ snake.py
@@ -17,17 +17,23 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             previous_seg_position = self.segments[seg_num - 1].position()
             self.segments[seg_num].goto(previous_seg_position)
         self.head.forward(MOVE_DISTANCE)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def up(self):
         if self.head.heading() != DOWN:
